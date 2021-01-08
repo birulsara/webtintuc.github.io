b57b104a81f61ff9e2997d2f8118f635ea3f94e5
Khai <nguyenquockhai0286@gmail.com>
2021-01-08 14:53:17 +0700

    first commit

---
+++ resources/views/news/partials/footer.blade.php
@@ -0,0 +1,42 @@
+<footer class="row clearfix">
+	<!-- Footer widgets -->
+	<ul class="no-bullet clearfix">
+		<li class="widget four column">
+			<h3 class="widget-title">Thông Tin</h3>
+        	<div class="textwidget">
+        		<img alt="" src="images/logo/logo.png">
+        		<p>BTL Trang Tin Tức Công Nghệ.</p>
+        		<p>GV Hướng Dẫn Nguyễn Quốc Trung.</p>
+       		</div>
+		</li>
+		<li class="widget four column">
+			<h3 class="widget-title">Danh Mục Chính</h3>
+			<div class="twitter-widget">
+        		<ul class="arrow-list">
+        			<li><a href="category/cong-nghe">Tin Công Nghệ</a></li>
+        			<li><a href="category/thu-thuat">Thủ Thuật</a></li>
+        			<li><a href="category/su-kien">Sự Kiện</a></li>
+        		</ul>
+        	</div>
+		</li>
+		<li class="widget four column">
+			<h3 class="widget-title">Liên Hệ</h3>
+        	<div class="twitter-widget">
+        		<ul class="arrow-list">
+        			<li><a href="#">Nguyễn Quốc Khải</a></li>
+        			<li><a href="#">Nguyễn Quốc Khánh</a></li>	
+        			<li><a href="#">GVHD: Nguyễn Quốc Trung</a></li>
+        		</ul>
+        	</div>
+		</li>
+	</ul>
+	<!-- End Footer widgets -->
+
+	<div class="copyright clearfix">
+		© Copyright 2021 HVKT Mật Mã
+	</div>
+
+	<div id="back-to-top" class="right">
+		<a href="#top">Back to Top</a>
+	</div>
+</footer>
